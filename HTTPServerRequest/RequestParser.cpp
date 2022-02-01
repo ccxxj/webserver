2540676f4b26f30e00f63ab2c10e864703e52cc9
Olga Samara <olcha.olcha@gmail.com>
2022-02-01 16:28:45 +0100

    parsing the request line

---
+++ HTTPServerRequest/RequestParser.cpp
@@ -0,0 +1,43 @@
+#include "RequestParser.hpp"
+
+// RequestParser::StateDispatcher RequestParser::dispatch_table[] = {
+//     {METHOD, RequestParser::parse_method},
+//     {REQUEST_URI, RequestParser::parse_request_uri},
+//     {HTTP_VERSION, RequestParser::parse_HTTP_version},
+//     {HEADERS, RequestParser::parse_headers},
+//     {MESSAGE_BODY, RequestParser::parse_message_body},
+// };
+
+// TODO: add check on the bytes read == -1 or == 0(if the client stopped the connection)
+
+RequestParser::RequestParser() {}
+
+RequestParser::~RequestParser(){}
+
+// TODO: replace with the state machine?
+void RequestParser::parse_HTTP_request(char* buffer) {
+    buffer = request_reader.read_line(buffer);
+    _parse_request_line(request_reader.get_accumulator());
+    buffer = request_reader.read_line(buffer);
+    _parse_headers(request_reader.get_accumulator());
+}
+
+std::string& RequestParser::_get_substring(size_t* start, const std::string& string) {
+    const std::string delimiter = " ";
+    size_t match = string.find(delimiter);
+    if (match == std::string::npos) {
+        return; //TODO: fill response error
+    }
+    std::string substring = string.substr(0, match);
+    *start = match;
+    return substring;
+}
+
+void RequestParser::_parse_request_line(const std::string& accumulating_string) {
+    size_t start  = 0;
+    std::string& method = _get_substring(&start, accumulating_string);
+    http_request_message.set_method(method);
+    std::string& request_uri = _get_substring(&start, accumulating_string);
+    http_request_message.set_request_uri(request_uri); //TODO: why am I unable to swap?
+    request_reader.get_accumulator().swap(http_request_message.get_HTTP_version());
+}
---
+++ HTTPServerRequest/RequestParser.hpp
@@ -0,0 +1,64 @@
+#ifndef REQUESTPARSER_HPP
+#define REQUESTPARSER_HPP
+
+#include <string>
+#include <set>
+#include <vector>
+#include <iostream> // TODO: remove
+
+#include "RequestReader.hpp"
+#include "HTTPRequestMessage.hpp"
+
+class RequestParser {
+
+public:
+    RequestParser();
+    RequestParser(const RequestParser& other);
+    ~RequestParser();
+    const RequestParser& operator=(const RequestParser& other);
+
+    void parse_HTTP_request(char* buffer);
+
+// TODO: check the smaller fucntionality:
+private:
+    RequestReader request_reader;
+    HTTPRequestMessage http_request_message;
+
+    void _parse_request_line(const std::string& accumulating_string);
+    void _parse_headers(const std::string& accumulaing_string);
+
+    std::string& _parse_method(const std::string& message_buffer);
+    std::string& parse_request_uri(const std::string& message_buffer);
+    std::string& parse_HTTP_version(const std::string& message_buffer);
+    std::string& parse_message_body(const std::string& message_buffer);
+
+   std::string& _get_substring(size_t* start, const std::string& string);
+
+private:
+    enum State {
+        METHOD = 0,
+        REQUEST_URI,
+        HTTP_VERSION,
+        HEADERS,
+        MESSAGE_BODY,
+        NUM_STATES
+    };
+
+    enum TriggerEvent {
+        SPACE_FOUND = 0,
+        NEWLINE_FOUND,
+        EMPTY_LINE_FOUND,
+        NUM_EVENTS
+    };
+
+    struct StateDispatcher {
+        enum State state;
+        std::string& (*ptr)(const std::string& value);
+    };
+
+    static StateDispatcher state_dispatch_table[];
+
+};
+
+
+#endif
