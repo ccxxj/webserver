a565fdb0a812659b59bb89d4fb8b7e5b330cb6d7
Mollie-S <olcha.olcha@gmail.com>
2022-02-01 09:51:06 +0100

    updates

--- HttpRequest/RequestReader.cpp
+++ HttpRequest/RequestReader.cpp
@@ -1,5 +1,8 @@
 #include "RequestReader.hpp"
 
+#include "StatusCodes.hpp"
+
+
 RequestReader::RequestReader(/* args */)
 {
 }
@@ -8,22 +11,22 @@ RequestReader::~RequestReader()
 {
 }
 
-bool RequestReader::_is_end_of_request_line(char* current_string) {
-    return *current_string == '\n';
+bool RequestReader::_is_end_of_line(char* current_string) {
+    return *current_string == '\r';
 }
-bool RequestReader::_is_end_of_header_fields(char* current_string, std::string& accumulating_string, size_t acc_length) {
+bool RequestReader::_is_end_of_header_fields(char* current_string, std::string& accumulating_string) {
     return accumulating_string[acc_length - 1] == '\r' && *current_string == '\n';
 }
 
-char* RequestReader::_read_request_line(char* buffer, std::string& result, size_t* result_length) {
-    while(!_is_end_of_request_line(buffer)) {
-        if (*result_length < MAX_SIZE_BODY) {
+char* RequestReader::_read_request_line(char* buffer, std::string& result) {
+    while(!_is_end_of_line(buffer)) {
+        if (RequestReader::_length_counter > MAX_SIZE_BODY) {
             return; // TODO:: replace by error code
         }
         if (isascii(*buffer)) {
             result.append(1, *buffer);
             buffer++;
-            result_length++;
+            RequestReader::_length_counter++;
         }
         else {
             std::cout << "Invalid character!" << std::endl; // TODO:: replace by error code
@@ -32,8 +35,7 @@ char* RequestReader::_read_request_line(char* buffer, std::string& result, size_
 }
 
 std::string& RequestReader::read_HTTP_request(char* buffer, std::string& result) {
-    size_t result_length = result.length();
-    buffer = _read_request_line(buffer, result, &result_length);
+    buffer = _read_request_line(buffer, result);
     buffer = _read_header_fields(buffer, result, &result_length);
     while(!_is_end_of_header_fields(buffer, result, result_length) && result_length < MAX_SIZE_BODY) {
         if (isascii(*buffer)) {
--- HttpRequest/RequestReader.hpp
+++ HttpRequest/RequestReader.hpp
@@ -10,12 +10,14 @@
 class RequestReader
 {
 private:
-	bool _is_end_of_request_line(char* current_string);
-    bool _is_end_of_header_fields(char* current_string, std::string& accumulating_string, size_t acc_length);
+	bool _is_end_of_line(char *current_string);
+	bool _is_end_of_header_fields(char *current_string, std::string &accumulating_string);
 
-    char* _read_request_line(char* buffer, std::string& result, size_t* result_length);
+	char* _read_request_line(char* buffer, std::string& result);
 
     std::string& read_HTTP_request(char* buffer, std::string& result);
+
+	static size_t _length_counter;
 
 public:
 	RequestReader(/* args */);
