b40c0b3ab5e3e5caea01333d184e57f9a91ced88
Olga Samara <olcha.olcha@gmail.com>
2022-02-01 16:31:41 +0100

    removed doubling directory

--- HttpRequest/HTTPRequestMethods.cpp
+++
@@ -1,9 +0,0 @@
-#include "HTTPRequestMethods.hpp"
-
-namespace HTTPRequest {
-
-	namespace {
-		const char* existing_methods_list[] = {"GET", "POST", "HEAD", "DELETE", "PUT", "CONNECT", "OPTIONS", "TRACE", "PATCH"};
-	}
-    std::set<std::string> HTTPRequestMethods(existing_methods_list, existing_methods_list + sizeof(existing_methods_list) / sizeof(existing_methods_list[0]));
-}
