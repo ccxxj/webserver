abf474995776619ca31cf41d0e8922a7cca4273d
Mollie-S <olcha.olcha@gmail.com>
2022-02-03 15:15:59 +0100

    split_line() added, headers parsing added

--- HTTPServerRequest/RequestParser.cpp
+++ HTTPServerRequest/RequestParser.cpp
@@ -15,31 +15,61 @@ RequestParser::RequestParser() {}
 RequestParser::~RequestParser(){}
 
 // TODO: replace with the state machine?
-void RequestParser::parse_HTTP_request(char* buffer) {
-    buffer = request_reader.read_line(buffer);
-    _parse_request_line(request_reader.get_accumulator());
-    // buffer = request_reader.read_line(buffer);
-    // _parse_headers(request_reader.get_accumulator());
-}
-
-
-std::string RequestParser::_get_substring(size_t* start, const std::string& string) {
-    const std::string delimiter = " ";
-    size_t match = string.find(delimiter);
-    if (match == std::string::npos) {
-        return NULL; //TODO: fill response error
+// TODO: check for any whitespaces which are not allowed
+void RequestParser::parse_HTTP_request(char* buffer, size_t bytes_read) {
+    char *message_end = buffer + bytes_read;
+    std::string accumulator = request_reader.read_line(&buffer, message_end);
+    _parse_request_line(accumulator);
+    //TODO: validate request line
+    while(buffer != message_end) {
+        if (request_reader._is_end_of_header_fields(buffer, message_end)){
+            buffer += 4; // skipping double \r\n
+            break;
+        }
+        accumulator = request_reader.read_line(&buffer, message_end);
+        _parse_header(accumulator);
     }
-    std::string substring = string.substr(*start, match);
-    *start = match;
-    return substring;
+    //TODO: validate headers
 }
 
-void RequestParser::_parse_request_line(const std::string& accumulating_string) {
+std::vector<std::string> split_line(const std::string& line, const char delimiter){
     size_t start  = 0;
-    std::string method = _get_substring(&start, accumulating_string);
-    http_request_message.set_method(method);
-    std::string request_uri = _get_substring(&start, accumulating_string);
-    http_request_message.set_request_uri(request_uri);
-    std::string HTTPversion = accumulating_string.substr(start, accumulating_string.size());
-    http_request_message.set_HTTP_version(HTTPversion);
+    std::vector<std::string> lines;
+
+    while (true)
+    {
+        size_t match = line.find(delimiter, start);
+        if (match == std::string::npos) {
+            break; //TODO: fill response error
+        }
+        char ch = line[match];
+        size_t len = match - start;
+        lines.push_back(line.substr(start, len));
+        start = match + 1;
+    }
+    lines.push_back(line.substr(start, line.size()));
+    return lines;
+}
+
+//TODO: invalid request line results in 400 (Bad Request) response
+void RequestParser::_parse_request_line(const std::string& accumulating_string) {
+    if (accumulating_string == "")
+        return; //TODO: fill error response
+    std::vector<std::string> segments = split_line(accumulating_string, ' ');
+    http_request_message.set_method(segments[0]);
+    if (segments[1].size() > 2000) {
+        return; //TODO: respond with 414 (URI Too Long) status code
+    }
+    http_request_message.set_request_uri(segments[1]);
+    http_request_message.set_HTTP_version(segments[2]);
+}
+
+void RequestParser::_parse_header(const std::string& accumulating_string) {
+        if (accumulating_string == "")
+        return; //TODO: fill error response
+    std::vector<std::string> segments = split_line(accumulating_string, ':');
+    std::pair<std::string, std::string> header_field(segments[0], segments[1]); //TODO: No whitespace is allowed between the header field-name and colon.
+    http_request_message.set_header_field(header_field); //TODO: what if the header name exists?
+    // std::cout << "header name: " << segments[0] 
+    //           << "      header value: " << segments[1] << std::endl;
 }
--- HTTPServerRequest/RequestParser.hpp
+++ HTTPServerRequest/RequestParser.hpp
@@ -17,46 +17,48 @@ public:
     ~RequestParser();
     const RequestParser& operator=(const RequestParser& other);
 
-    void parse_HTTP_request(char* buffer);
+    void parse_HTTP_request(char* buffer, size_t bytes_read);
     HTTPRequestMessage http_request_message;
 
-// TODO: check the smaller fucntionality:
+
 private:
     RequestReader request_reader;
 
     void _parse_request_line(const std::string& accumulating_string);
-    void _parse_headers(const std::string& accumulaing_string);
+    void _parse_header(const std::string& accumulating_string);
 
-    std::string& _parse_method(const std::string& message_buffer);
+    std::vector<std::string> split(const std::string& accumulating_string);
+    
+    std::string &_parse_method(const std::string &message_buffer);
     std::string& parse_request_uri(const std::string& message_buffer);
     std::string& parse_HTTP_version(const std::string& message_buffer);
     std::string& parse_message_body(const std::string& message_buffer);
 
    std::string _get_substring(size_t* start, const std::string& string);
 
-private:
-    enum State {
-        METHOD = 0,
-        REQUEST_URI,
-        HTTP_VERSION,
-        HEADERS,
-        MESSAGE_BODY,
-        NUM_STATES
-    };
-
-    enum TriggerEvent {
-        SPACE_FOUND = 0,
-        NEWLINE_FOUND,
-        EMPTY_LINE_FOUND,
-        NUM_EVENTS
-    };
-
-    struct StateDispatcher {
-        enum State state;
-        std::string& (*ptr)(const std::string& value);
-    };
-
-    static StateDispatcher state_dispatch_table[];
+// private:
+//     enum State {
+//         METHOD = 0,
+//         REQUEST_URI,
+//         HTTP_VERSION,
+//         HEADERS,
+//         MESSAGE_BODY,
+//         NUM_STATES
+//     };
+// 
+//     enum TriggerEvent {
+//         SPACE_FOUND = 0,
+//         NEWLINE_FOUND,
+//         EMPTY_LINE_FOUND,
+//         NUM_EVENTS
+//     };
+// 
+//     struct StateDispatcher {
+//         enum State state;
+//         std::string& (*ptr)(const std::string& value);
+//     };
+// 
+//     static StateDispatcher state_dispatch_table[];
 
 };
 
