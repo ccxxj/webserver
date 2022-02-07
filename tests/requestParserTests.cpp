ba8aec9e69726ceeebc1f4ec225af958014205b2
Olga Samara <olcha.olcha@gmail.com>
2022-02-07 19:20:25 +0100

    http request messages are set from a file

--- tests/requestParserTests.cpp
+++ tests/requestParserTests.cpp
@@ -1,5 +1,7 @@
-#include <string>
 #include "catch_amalgamated.hpp"
+
+#include <string>
+#include <fstream>
 
 #include "../src/HTTP/RequestHandler.hpp"
 #include "../src/HTTPRequest/RequestParser.hpp"
@@ -23,7 +25,23 @@ char http_request_mes[] = "POST /cgi-bin/process.cgi HTTP/1.1\r\n"
 
                             "licenseID=string&content=string&/paramsXML=string";
 
-    TEST_CASE ("Simple generator use") {
+TEST_CASE ("Simple generator use") {
+    std::vector<char*> http_requests;
+    std::string buffer;
+    std::ifstream file("requestParserMessages.txt");
+    if (file.is_open()) {
+        std::string line;
+        for( std::string line; getline(file, line); ) {
+            while (line != "---end---") {
+                buffer.append(line);
+                buffer.append("\r\n");
+            }
+            char* buf = &buffer[0];
+            http_requests.push_back(buf);
+            getline(file, line); // skipping the newline after the end
+        }
+        file.close();
+    }
         auto request = GENERATE(http_request_mes);
         parser.parse_HTTP_request(http_request_mes, strlen(http_request_mes));
         CHECK(_http_request_message.get_method() == "POST");
