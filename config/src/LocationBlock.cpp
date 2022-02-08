ab9eeef89dfeb1a2d287f023360d3bcc24ef854f
Xiaojing Xu <xxu@f1r6s4.codam.nl>
2022-02-08 10:40:19 +0100

    add changes to the locationblock.cpp

--- config/src/LocationBlock.cpp
+++ config/src/LocationBlock.cpp
@@ -1,4 +1,5 @@
 #include "LocationBlock.hpp"
+#include "Utils.hpp"
 
 LocationBlock::LocationBlock(/* args */)
 {
@@ -8,10 +9,25 @@ LocationBlock::~LocationBlock()
 {
 }
 
-// LocationBlock::LocationBlock(/* args */){}
-// LocationBlock::~LocationBlock(){}
-// void LocationBlock::set_route(std::string str);
-// void LocationBlock::set_limit_except(std::string str);
-// std::string LocationBlock::get_route(std::string str) const;
-// std::vector<std::string> LocationBlock::get_limit_except(void) const;
+void LocationBlock::set_route(std::string str)
+{
+	int first = str.find_first_not_of("     ");
+    int last = str.find_first_of("  {", first);
+    str = str.substr(first, last - first);
+}
+
+void LocationBlock::set_limit_except(std::string str)
+{
+    
+}
+
+std::string LocationBlock::get_route(std::string str) const
+{
+    return _route;
+}
+
+std::vector<std::string> LocationBlock::get_limit_except(void) const
+{
+    return _limit_except;
+}
 
