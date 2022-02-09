ecb1dd32e15b22222b20c95a442281073915d64f
Olga Samara <olcha.olcha@gmail.com>
2022-02-09 19:12:10 +0100

    setting error status in response message

--- src/HttpResponse/ResponseMessage.cpp
+++ src/HttpResponse/ResponseMessage.cpp
@@ -4,4 +4,14 @@ namespace HTTPResponse {
     ResponseMessage::ResponseMessage() : _HTTP_version("HTTP/1.1"), _status_code(""), _reason_phrase(""){}
 
     ResponseMessage::~ResponseMessage() {}
+
+    void ResponseMessage::set_status_code(const std::string& code) {
+        _status_code = code;
+    }
+    
+    void ResponseMessage::set_status_code(const std::string& reason) {
+        _reason_phrase = reason;
+    }
+    
+
 }
