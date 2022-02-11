5b8d21386d7e81613dbc72a805830f83cb2d4e50
ergunirem <icikrikc@f1r5s7.codam.nl>
2022-02-11 20:10:45 +0100

    main and webserver.cpp adjusted to config parser but connection between Server class and ConfigData is still missing

--- src/Webserver.cpp
+++ src/Webserver.cpp
@@ -1,5 +1,5 @@
-#include "Webserver.hpp"
-Webserver::Webserver(/* args */)
+#include "../include/Webserver.hpp"
+Webserver::Webserver(std::string file_path): _file_path(file_path)
 {
 }
 
@@ -12,6 +12,17 @@ void Webserver::start() {
 		// if (config is ok) {
 		// HTTPServer server(&config);
 	// }
+	try
+	{
+		Config::ConfigData config;
+		Config::ConfigParser parser(&config, _file_path);
+		parser.parse();
+		config.print_servers_info();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 		HTTP::Server server;
 		server.run();
 	}
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
-#include "Webserver.hpp"
+#include "../include/Webserver.hpp"
+#include <iostream>
+#define DEFAULT_CONFIG "./example_config_files/default-conf"
 
-int main(void) {
-	Webserver webserver;
+int main(int argc, char **argv) {
+	std::string file_path;
+
+	if (argc > 2)
+	{
+		std::cerr << "Invalid number of arguments" << std::endl;
+		return (1); //TODO return EXIT_FAILURE?
+	}
+	if (argc == 1)
+		file_path = DEFAULT_CONFIG;
+	else if (argc == 2)
+		file_path = argv[1];
+
+	Webserver webserver(file_path);
 	webserver.start();
 	return 0;
 }
