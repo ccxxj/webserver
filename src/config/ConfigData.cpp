43009732b4b1d914123c17075be44d98984ac905
ergunirem <icikrikc@f1r5s7.codam.nl>
2022-02-11 20:09:14 +0100

    cnofig added src but config's own main and Makefile is still there

---
+++ src/config/ConfigData.cpp
@@ -0,0 +1,156 @@
+#include "ConfigData.hpp"
+#define B_RED "\033[1;31m"
+#define MAGENTA "\033[36m"
+#define PURPLE "\033[35m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[32m"
+#define BLUE "\033[34m"
+#define RESET "\033[0m"
+
+namespace Config
+{
+
+    ConfigData::ConfigData(/* args */)
+    {
+        // std::cout << "ConfigData default construtor" << std::endl;
+    }
+
+    ConfigData::ConfigData(const ConfigData &other)
+    {
+        // std::cout << "ConfigData copy construtor" << std::endl;
+        *this = other;
+    }
+
+    const ConfigData &ConfigData::operator=(const ConfigData &other)
+    {
+        // std::cout << "ConfigData assign operator" << std::endl;
+        _servers = other._servers;
+        return *this;
+    }
+
+    ConfigData::~ConfigData()
+    {
+        // std::cout << "ConfigData destructor" << std::endl;
+    }
+
+    void ConfigData::make_first_server_default()
+    {
+        if (_servers.size())
+            _servers[0].set_default(true);
+    }
+
+    std::vector<ServerBlock> &ConfigData::get_servers(void)
+    {
+        return (_servers);
+    }
+
+    void ConfigData::print_listen_info(ServerBlock &server)
+    {
+        std::vector<std::string> ports = server.get_listen();
+        std::cout << GREEN << "listening ports: ";
+        for (size_t i = 0; i < ports.size(); i++)
+        {
+            std::cout << i + 1 << ")" << ports[i] << " ";
+        }
+        std::cout << RESET << std::endl;
+    }
+
+    void ConfigData::print_roots(ServerBlock &server)
+    {
+
+        std::vector<std::string> roots = server.get_root();
+        std::cout << YELLOW << "roots: ";
+        for (size_t i = 0; i < roots.size(); i++)
+        {
+            std::cout << i + 1 << ")" << roots[i] << " ";
+        }
+        std::cout << RESET << std::endl;
+    }
+
+    void ConfigData::print_returns(ServerBlock &server)
+    {
+
+        std::vector<std::string> returns = server.get_return();
+        std::cout << PURPLE << "returns: ";
+        for (size_t i = 0; i < returns.size(); i++)
+        {
+            std::cout << i + 1 << ")" << returns[i] << " ";
+        }
+        std::cout << RESET << std::endl;
+    }
+
+    void ConfigData::print_error_pages(ServerBlock &server)
+    {
+
+        std::vector<std::string> error_pages = server.get_error_page();
+        std::cout << "\033[34m"
+                  << "error_pages: ";
+        for (size_t i = 0; i < error_pages.size(); i++)
+        {
+            std::cout << i + 1 << ")" << error_pages[i] << " ";
+        }
+        std::cout << RESET << std::endl;
+    }
+
+    void ConfigData::print_server_name(ServerBlock &server)
+    {
+
+        std::vector<std::string> names = server.get_server_name();
+        std::cout << MAGENTA << "server_names: ";
+        for (size_t i = 0; i < names.size(); i++)
+        {
+            std::cout << i + 1 << ")" << names[i] << " ";
+        }
+        std::cout << RESET << std::endl;
+    }
+
+    void ConfigData::print_limit_except(LocationBlock &location)
+    {
+
+        std::vector<std::string> limits = location.get_limit_except();
+        std::cout << MAGENTA << "\tmethods: ";
+        for (size_t i = 0; i < limits.size(); i++)
+        {
+            std::cout << i + 1 << ")" << limits[i] << " ";
+        }
+        std::cout << RESET << std::endl;
+    }
+
+    void ConfigData::print_multiple_locations_info(ServerBlock &server)
+    {
+        std::vector<LocationBlock> locations = server.get_location();
+        for (size_t i = 0; i < locations.size(); i++)
+        {
+            std::cout << RED << "\n\tLocation Number " << i + 1 << RESET << std::endl;
+            std::cout << GREEN << "\troute: " << locations[i].get_route() << std::endl;
+            std::cout << BLUE << "\tauto_index: " << locations[i].get_autoindex() << RESET << std::endl;
+            print_limit_except(locations[i]);
+            std::cout << "\t";
+            print_roots((ServerBlock &)locations[i]);
+            std::cout << "\t";
+            print_returns((ServerBlock &)locations[i]);
+            std::cout << "\t";
+            print_error_pages((ServerBlock &)locations[i]);
+        }
+    }
+
+    void ConfigData::print_servers_info(void)
+    {
+        for (size_t i = 0; i < _servers.size(); i++)
+        {
+            std::cout << B_RED << "\nServer Number " << i + 1;
+            if (_servers[i].get_default())
+                std::cout << " (default)";
+            std::cout << RESET << std::endl;
+            print_listen_info(_servers[i]);
+            print_server_name(_servers[i]);
+            print_roots(_servers[i]);
+            std::cout << GREEN << "client_max_body_size: " << _servers[i].get_client_max_body_size() << RESET << std::endl;
+            print_returns(_servers[i]);
+            print_error_pages(_servers[i]);
+            print_multiple_locations_info(_servers[i]);
+        }
+    }
+
+} // namespace Config
---
+++ src/config/ServerBlock.cpp
@@ -0,0 +1,88 @@
+#include "ServerBlock.hpp"
+#include "Utils.hpp"
+
+namespace Config
+{
+
+    ServerBlock::ServerBlock(/* args */)
+    {
+        _is_default = false;
+        // std::cout << "ServerBlock construtor" << std::endl;
+    }
+
+    ServerBlock::ServerBlock(const ServerBlock &other)
+    {
+        // std::cout << "ServerBlock copy constructor" << std::endl;
+        *this = other;
+    }
+
+    const ServerBlock &ServerBlock::operator=(ServerBlock const &other)
+    {
+        _is_default = other._is_default;
+        _client_max_body_size = other._client_max_body_size;
+        _listen = other._listen;
+        _server_name = other._server_name;
+        _locations = other._locations;
+        _root = other._root;
+        _return = other._return;
+        _error_page = other._error_page;
+        // std::cout << "ServerBlock assign operator" << std::endl;
+        return *this;
+    }
+
+    ServerBlock::~ServerBlock()
+    {
+        //std::cout  << "ServerBlock destructor" << std::endl;
+    }
+
+    void ServerBlock::set_listen(std::string str)
+    {
+        Utils::remove_first_keyword(str);
+        Utils::split_value(str, _listen);
+    }
+
+    void ServerBlock::set_server_name(std::string str)
+    {
+        Utils::remove_first_keyword(str);
+        Utils::split_value(str, _server_name);
+    }
+
+    void ServerBlock::set_client_max_body_size(std::string str)
+    {
+        Utils::remove_first_keyword(str);
+        int first = str.find_first_not_of("     ;");
+        int last = str.find_first_of("     ;", first + 1);
+        std::string temp = str.substr(first, last - first);
+        _client_max_body_size = stoi(temp);
+    }
+
+    void ServerBlock::set_default(bool value)
+    {
+        _is_default = value;
+    }
+
+    int ServerBlock::get_client_max_body_size(void) const
+    {
+        return _client_max_body_size;
+    }
+
+    std::vector<std::string> ServerBlock::get_listen() const
+    {
+        return _listen;
+    }
+
+    std::vector<std::string> ServerBlock::get_server_name() const
+    {
+        return _server_name;
+    }
+
+    std::vector<LocationBlock> &ServerBlock::get_location(void)
+    {
+        return _locations;
+    }
+
+    bool ServerBlock::get_default() const
+    {
+        return _is_default;
+    }
+} // namespace Config
