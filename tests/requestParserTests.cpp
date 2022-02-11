33369f7889953e357d0026c094baf0f9fc7fd514
Olga Samara <olcha.olcha@gmail.com>
2022-02-11 15:51:29 +0100

    parsing tests fixed

--- tests/requestParserTests.cpp
+++ tests/requestParserTests.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <fstream>
+#include <map>
 
 #include "../src/HTTP/RequestHandler.hpp"
 #include "../src/HTTPRequest/RequestParser.hpp"
@@ -10,53 +11,66 @@
 
 namespace tests {
 
-HTTPRequest::RequestMessage _http_request_message;
-HTTPResponse::ResponseMessage _http_response_message;
-HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-
-char http_request_mes[] = R"(POST /cgi-bin/process.cgi HTTP/1.1
-                            User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
-                            Host: www.tutorialspoint.com
-                            Content-Type: application/x-www-form-urlencoded
-                            Content-Length: length
-                            Accept-Language: en-us
-                            Accept-Encoding: gzip, deflate
-                            Connection: Keep-Alive
-
-                            licenseID=string&content=string&/paramsXML=string
-                            )";
-
-    TEST_CASE ("Simple generator use") {
-        std::vector<char*> http_requests;
-        std::string buffer = "";
-
-        std::ifstream file("/Users/osamara/codam_core/webserver/tests/requestParserMessages.txt");
+    std::vector<std::string> http_requests;
+    void fill_requests() {
+        std::ifstream file("tests/requestParserMessages.txt");
         if (file.is_open()) {
             std::string line;
+            std::string buffer = "";
             while( file) {
                 getline(file, line);
-                if (line == "---end---") {
-                    char* buf = &buffer[0];
-                    http_requests.push_back(buf);
-                    getline(file, line); // skipping the newline after the end
+                if (line == "------") {
+                    if (buffer != "") {
+                        http_requests.push_back(buffer);
+                        buffer.resize(0);
+                    }
+                    getline(file, line); // skipping the line with index number, we don't need it
                 }
-                buffer.append(line);
-                buffer.append("\r\n");
-            
+                else {
+                    buffer.append(line);
+                    buffer.append("\r\n");
+                }
             }
             file.close();
         }
+    }
+
+    TEST_CASE ("Parsing valid request messages") {
+        fill_requests();
+        HTTPRequest::RequestMessage _http_request_message;
+        HTTPResponse::ResponseMessage _http_response_message;
+        HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
+        char* buf = &(http_requests[0])[0];
+        parser.parse_HTTP_request(buf, strlen(buf));
+
+            SECTION("Request line should be parsed and split into 3 atrributes") {
+                CHECK(_http_request_message.get_method() == "POST");
+                CHECK(_http_request_message.get_request_uri() == "/cgi-bin/process.cgi");
+                CHECK(_http_request_message.get_HTTP_version() == "HTTP/1.1");
+                
+                    SECTION("Headers must be split by ':' and headers map must be filled") {
+                        CHECK(_http_request_message.get_headers().size() == 7);
+                        CHECK(_http_request_message.get_header_value("Accept-Language") == "en-us");
+                    }
+
+                    SECTION("Any number of spaces are allowed in header_values (after ''), they must be trimmed") {
+                        CHECK(_http_request_message.get_header_value("Content-Type") == "application/x-www-form-urlencoded");
+                    }
+            }
+    }
+    TEST_CASE ("Parsing non_existing headers, map must be empty") {
+        HTTPRequest::RequestMessage _http_request_message;
+        HTTPResponse::ResponseMessage _http_response_message;
+        HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
+        char* buf = &(http_requests[7])[0];
+        parser.parse_HTTP_request(buf, strlen(buf));    
+        CHECK(_http_request_message.get_headers().size() == 0);            
+    }
+}
+
         // using IteratorType = std::vector<char*>::iterator;
         // std::vector<char*>::iterator it = http_requests.begin();
         // std::vector<char*>::iterator ite = http_requests.end();
         // auto request = GENERATE_REF(from_range(it, ite - 1));
         //  auto request = GENERATE_COPY(from_range(http_requests));
-
         // auto request = GENERATE(http_request_mes);
-
-        parser.parse_HTTP_request(http_request_mes, strlen(http_request_mes));
-        CHECK(_http_request_message.get_method() == "POST");
-        CHECK(_http_request_message.get_request_uri() == "/cgi-bin/process.cgi");
-        CHECK(_http_request_message.get_HTTP_version() == "HTTP/1.1");
-    }
-}
