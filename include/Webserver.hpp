2d125975aa4fdec819e1fa42ecde9daecaaece8e
ergunirem <icikrikc@f1r5s7.codam.nl>
2022-02-11 18:56:17 +0100

    main files moved to this branch

---
+++ include/Webserver.hpp
@@ -0,0 +1,16 @@
+#ifndef WEBSERVER_HPP
+#define WEBSERVER_HPP
+
+#include "../src/HTTP/Server.hpp"
+
+class Webserver
+{
+private:
+	/* data */
+public:
+	Webserver(/* args */);
+	~Webserver();
+	void start();
+};
+
+#endif
