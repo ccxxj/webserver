c404f4915bc8bb924b471d72a728e11d5abb22a3
Olga Samara <olcha.olcha@gmail.com>
2022-02-16 17:41:51 +0100

    Request method is set only if it is supported by the webserv

--- src/HTTPRequest/RequestParser.cpp
+++ src/HTTPRequest/RequestParser.cpp
@@ -2,6 +2,7 @@
 #include <algorithm> // for std::distance
 
 #include "../HTTP/Exceptions/RequestException.hpp"
+#include "HTTPRequestMethods.hpp"
 
 namespace HTTPRequest {
 
@@ -14,16 +15,16 @@ namespace HTTPRequest {
     // TODO: check for any whitespaces which are not allowed
     void RequestParser::parse_HTTP_request(char* buffer, size_t bytes_read) {
         char *message_end = buffer + bytes_read;
-        std::string accumulator = request_reader.read_line(&buffer, message_end);
-        _parse_request_line(accumulator);
+        _accumulator = _request_reader.read_line(&buffer, message_end);
+        _parse_request_line();
         //TODO: validate request line
         while(buffer != message_end) {
-            if (request_reader._is_end_of_header_fields(buffer, message_end)){
+            if (_request_reader._is_end_of_header_fields(buffer, message_end)){
                 buffer += 2; // skipping only one \r\n as the previous one has allrady been skipped in the previous iteration
                 break;
             }
-            accumulator = request_reader.read_line(&buffer, message_end);
-            _parse_header(accumulator);
+            _accumulator = _request_reader.read_line(&buffer, message_end);
+            _parse_header();
         }
         //TODO: validate headers
     }
@@ -46,14 +47,41 @@ namespace HTTPRequest {
         return lines;
     }
 
-    void RequestParser::_parse_request_line(const std::string& accumulating_string) {
-        if (accumulating_string.empty())
+    bool RequestParser::_comp(const std::string& lhs, const std::string& rhs) {
+        return (lhs.size() < rhs.size());
+    }
+
+    size_t RequestParser::_longest_method_size() {
+        size_t longest_size = 0;
+        for (std::set<std::string>::iterator it = HTTPRequestMethods.begin(); it != HTTPRequestMethods.end(); it++) {
+            size_t string_size = (*it).size();
+            if ( string_size > longest_size) {
+                longest_size = string_size;
+            }
+        }
+        return longest_size;
+    }
+
+    bool RequestParser::_is_method_supported(const std::string& method) {
+        if (method.size() > _longest_method_size()){
+            throw Exception::RequestException(HTTPResponse::NotImplemented);
+        }
+        if (HTTPRequestMethods.find(method) == HTTPRequestMethods.end()) {
             throw Exception::RequestException(HTTPResponse::BadRequest);
-        std::vector<std::string> segments = _split_line(accumulating_string, ' ');
+        }
+        return true;
+    }
+
+    void RequestParser::_parse_request_line() {
+        if (_accumulator.empty())
+            throw Exception::RequestException(HTTPResponse::BadRequest);
+        std::vector<std::string> segments = _split_line(_accumulator, ' ');
         if (segments.size() < 3) {
             throw Exception::RequestException(HTTPResponse::BadRequest);
         }
-        _http_request_message->set_method(segments[0]);
+        if (_is_method_supported(segments[0])) {
+            _http_request_message->set_method(segments[0]);
+        }
         if (segments[1].size() > 2000) {
             throw Exception::RequestException(HTTPResponse::URITooLong);
         }
@@ -61,10 +89,10 @@ namespace HTTPRequest {
         _http_request_message->set_HTTP_version(segments[2]);
     }
 
-    void RequestParser::_parse_header(const std::string& accumulating_string) {
-            if (accumulating_string == "")
+    void RequestParser::_parse_header() {
+            if (_accumulator == "")
             return; //TODO: fill error response
-        std::vector<std::string> segments = _split_line(accumulating_string, ':');
+        std::vector<std::string> segments = _split_line(_accumulator, ':');
         std::pair<std::string, std::string> header_field(segments[0], _trim(segments[1])); //TODO: No whitespace is allowed between the header field-name and colon.
         _http_request_message->set_header_field(header_field); //TODO: what if the header name exists?
     }
--- src/HTTPRequest/RequestParser.hpp
+++ src/HTTPRequest/RequestParser.hpp
@@ -16,24 +16,29 @@ namespace HTTPRequest {
     class RequestParser {
 
     private:
+        RequestReader _request_reader;
+        std::string _accumulator;
+
+        void _parse_request_line();
+        void _parse_header();
+        bool _is_method_supported(const std::string& method);
+        size_t _longest_method_size();
+
         RequestParser();
-        RequestReader request_reader;
-
-        void _parse_request_line(const std::string& accumulating_string);
-        void _parse_header(const std::string& accumulating_string);
-
         std::vector<std::string> _split_line(const std::string& line, const char delimiter);
         std::string _trim(const std::string& s);
 
+        bool _comp(const std::string& lhs, const std::string& rhs);
     public:
+        HTTPRequest::RequestMessage* _http_request_message;
+        HTTPResponse::ResponseMessage* _http_response_message;
+
         RequestParser(HTTPRequest::RequestMessage* http_request, HTTPResponse::ResponseMessage* http_response);
         RequestParser(const RequestParser& other);
         ~RequestParser();
         const RequestParser& operator=(const RequestParser& other);
 
         void parse_HTTP_request(char* buffer, size_t bytes_read);
-        HTTPRequest::RequestMessage* _http_request_message;
-        HTTPResponse::ResponseMessage* _http_response_message;
     };
 }
 
