a63afe911d3e1b499fba26360b6d126661626567
Mollie-S <olcha.olcha@gmail.com>
2022-02-18 23:06:25 +0100

    Reading from buffer to a data member instead of a stack variable

--- src/HTTPRequest/RequestReader.cpp
+++ src/HTTPRequest/RequestReader.cpp
@@ -1,46 +1,51 @@
 #include "RequestReader.hpp"
 
-// #include "StatusCodes.hpp"
+#include "../HTTP/Exceptions/RequestException.hpp"
 
-size_t RequestReader::_length_counter = 0; // TODO create namespace
+namespace HTTPRequest {
 
-RequestReader::RequestReader() {}
+    size_t RequestReader::_length_counter = 0; // TODO create namespace
 
-RequestReader::~RequestReader()
-{
-}
+    RequestReader::RequestReader() : _accumulator(""){}
 
-bool RequestReader::_is_end_of_line(char* buffer, char* message_end) { // also checking that the current char is part of the buffer
-    return (buffer != message_end && *buffer == '\r'
-            && buffer + 1 != message_end && *(buffer + 1) == '\n');
-}
-
-bool RequestReader::_is_end_of_header_fields(char* buffer, char* message_end) {
-    return _is_end_of_line(buffer, message_end);
-}
-
-std::string RequestReader::read_line(char** buffer, char* message_end) { // pointer to the buffer as we need to keep track of it
-    std::string accumulating_string = "";
-    char* temp_ptr = *buffer;
-    while (temp_ptr != message_end)
+    RequestReader::~RequestReader()
     {
-        if (RequestReader::_length_counter > MAX_SIZE_BODY) {
-            return ""; // TODO:: replace by error code response
-        }
-        if (_is_end_of_line(temp_ptr, message_end)) {
-            temp_ptr += 2; // skipping \r\n
-            break;
-        }
-        if (isascii(*temp_ptr)) {
-            accumulating_string.append(1, *temp_ptr);
-            temp_ptr++;
-            RequestReader::_length_counter++;
-        }
-        else {
-            std::cout << "Invalid character! num" << RequestReader::_length_counter << std::endl; // TODO:: replace by returning error code
-            break;
-        }
     }
-    *buffer = temp_ptr;
-    return accumulating_string;
+
+    bool RequestReader::_is_end_of_line(char* buffer, char* buffer_end) { // also checking that the current char is part of the buffer
+        return (buffer != buffer_end && *buffer == '\r'
+                && buffer + 1 != buffer_end && *(buffer + 1) == '\n');
+    }
+
+    bool RequestReader::_is_end_of_header_fields(char* buffer, char* buffer_end) {
+        return _is_end_of_line(buffer, buffer_end);
+    }
+
+    std::string RequestReader::read_line(char** buffer, char* buffer_end, bool* can_be_parsed) { // pointer to the buffer as we need to keep track of it
+        char* temp_ptr = *buffer;
+        while (temp_ptr != buffer_end)
+        {
+            if (RequestReader::_length_counter > MAX_SIZE_BODY) {
+                throw Exception::RequestException(HTTPResponse::ContentTooLarge);
+            }
+            if (_is_end_of_line(temp_ptr, buffer_end)) {
+                *can_be_parsed = true;
+                std::string line = _accumulator.substr(0, (temp_ptr - *buffer));
+                temp_ptr += 2; // skipping \r\n
+                *buffer = temp_ptr;
+                _accumulator.resize(0);
+                return line;
+            }
+            if (isascii(*temp_ptr)) {
+                _accumulator.append(1, *temp_ptr);
+                temp_ptr++;
+                RequestReader::_length_counter++;
+            }
+            else {
+                throw Exception::RequestException(HTTPResponse::BadRequest);
+
+            }
+        }
+        return _accumulator;
+    }
 }
