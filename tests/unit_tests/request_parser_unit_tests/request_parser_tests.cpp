de5506fabc181ada3fc4b6ecaefe66d873c4d042
Olga Samara <olcha.olcha@gmail.com>
2022-02-23 17:53:36 +0100

    char*buf is passed to the parser

--- tests/unit_tests/request_parser_unit_tests/request_parser_tests.cpp
+++ tests/unit_tests/request_parser_unit_tests/request_parser_tests.cpp
@@ -34,6 +34,14 @@ namespace tests {
         }
         return http_requests;
     }
+    
+    char* create_writable_buf(std::string request) {
+        std::string str = request;
+        char* buf = new char[str.size() + 1];
+        std::copy(str.begin(), str.end(), buf);
+        buf[str.size()] = '\0'; // don't forget the terminating 0
+        return buf;
+    }
 
     TEST_CASE ("Request Parser - valid", "[request_parser]") {
         std::vector<std::string> http_requests = fill_requests("request_parser_unit_tests/request_parser_messages.txt");
@@ -42,7 +50,8 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char* buf = &(http_requests[0])[0];
+            
+            char* buf = create_writable_buf(http_requests[0]);
             parser.parse_HTTP_request(buf, strlen(buf));
 
             SECTION("Request line should be parsed and split into 3 atrributes") {
@@ -59,42 +68,45 @@ namespace tests {
                         CHECK(_http_request_message.get_header_value("Content-Type") == "application/x-www-form-urlencoded");
                     }
             }
+            delete[] buf;
         }
 
         SECTION ("Request with no headers passed, map is empty", "[invalid_request]") {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char* buf = &(http_requests[1])[0];
+            char* buf = create_writable_buf(http_requests[1]);
             parser.parse_HTTP_request(buf, strlen(buf));
             CHECK(_http_request_message.get_headers().size() == 0);            
+            delete[] buf;
         }
 
         SECTION ("Request multiple headers with the same name", "[invalid_request]") {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char* buf = &(http_requests[2])[0];
+            char* buf = create_writable_buf(http_requests[2]);
             parser.parse_HTTP_request(buf, strlen(buf));  
             CHECK(_http_request_message.get_header_value("Accept") == "text/plain, text/html");
+            delete[] buf;
         }
-        SECTION ("Request message coming with a delay", "[valid_request]") {
+        // SECTION ("Request message coming with a delay", "[valid_request]") {
 
-            HTTPRequest::RequestMessage _http_request_message;
-            HTTPResponse::ResponseMessage _http_response_message;
-            HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            // char* buf = &(http_requests[0])[0];
-            size_t index = 0;
-            char* buf = &(http_requests[0])[index];
-            size_t buf_size = http_requests[0].size();
-            while (index < buf_size) {
-                parser.parse_HTTP_request(buf, 1);
-                index++;
-            }
-            SECTION("Request line should be parsed and split into 3 atrributes") {
-                CHECK(_http_request_message.get_method() == "POST");
-            }
-        }
+        //     HTTPRequest::RequestMessage _http_request_message;
+        //     HTTPResponse::ResponseMessage _http_response_message;
+        //     HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
+        //     size_t index = 0;
+        //     char* buf = create_writable_buf(http_requests[0]);
+        //     size_t buf_size = http_requests[0].size();
+        //     while (index < buf_size) {
+        //         parser.parse_HTTP_request(buf, 1);
+        //         index++;
+        //     }
+        //     SECTION("Request line should be parsed and split into 3 atrributes") {
+        //         CHECK(_http_request_message.get_method() == "POST");
+        //     }
+        //     delete[] buf;
+        // }
     }
 
     TEST_CASE ("Invalid requests - exceptions thrown", "[request_parser]") {
@@ -103,7 +115,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[0])[0]; // header looks like this: "Host : localhost:80"
+            char* buf = create_writable_buf(http_requests[0]); // header looks like this: "Host : localhost:80"
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -111,7 +123,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[1])[0]; // P O ST /cgi-bin/process.cgi HTTP/1.1
+            char* buf = create_writable_buf(http_requests[1]); // P O ST /cgi-bin/process.cgi HTTP/1.1
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -119,7 +131,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[2])[0]; // POST /cgi-bin/ process.cgi HTTP/1.1
+            char* buf = create_writable_buf(http_requests[2]); // POST /cgi-bin/ process.cgi HTTP/1.1
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -127,7 +139,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[3])[0];
+            char* buf = create_writable_buf(http_requests[3]);
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -135,7 +147,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[4])[0]; //POSTT /foo.php HTTP/1.1
+            char* buf = create_writable_buf(http_requests[4]); //POSTT /foo.php HTTP/1.1
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -143,7 +155,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[5])[0]; // GET - without uri and version 
+            char* buf = create_writable_buf(http_requests[5]); // GET - without uri and version 
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -151,7 +163,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[6])[0]; // GET HTTP/1.1 
+            char* buf = create_writable_buf(http_requests[6]); // GET HTTP/1.1 
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
@@ -159,7 +171,7 @@ namespace tests {
             HTTPRequest::RequestMessage _http_request_message;
             HTTPResponse::ResponseMessage _http_response_message;
             HTTPRequest::RequestParser parser(&_http_request_message, &_http_response_message);
-            char *buf = &(http_requests[7])[0]; // long uri
+            char* buf = create_writable_buf(http_requests[7]); // long uri
             
             CHECK_THROWS_AS((parser.parse_HTTP_request(buf, strlen(buf))), ::Exception::RequestException);
         }
