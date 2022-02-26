41794cbc68211c70a4a38c14f76dac28f72bc802
ergunirem <iremthedeveloper@gmail.com>
2022-02-26 18:25:41 +0300

    validating server block openning bracket

--- src/config/ConfigValidator.cpp
+++ src/config/ConfigValidator.cpp
@@ -3,7 +3,7 @@
 namespace Config
 {
 
-	ConfigValidator::ConfigValidator(std::string file_path) :file_path(file_path)
+	ConfigValidator::ConfigValidator(std::string file_path) :_file_path(file_path)
 	{
 	}
 
@@ -11,42 +11,64 @@ namespace Config
 	{
 	}
 
-	void ConfigValidator::open_and_read_file(void)
+	void ConfigValidator::_open_and_read_file(void)
 	{
 		std::ifstream file_stream;
 		std::string line;
 
-		file_stream.open(file_path.c_str());
+		file_stream.open(_file_path.c_str());
 		if (!file_stream.is_open())
 			throw ConfigException("Configuration file failed to open");
 		while (std::getline(file_stream, line))
-			file_content.append(line + "\n");
+			_file_content.append(line + "\n");
 		file_stream.close();
 	}
 
-	void ConfigValidator::remove_comments(void)
+	void ConfigValidator::_remove_comments(void)
 	{
-		std::istringstream stream(file_content);
+		std::istringstream stream(_file_content);
 		std::string line;
 		size_t hashtag_pos;
 
-		file_content.clear();
+		_file_content.clear();
 		while (std::getline(stream, line))
 		{
 			hashtag_pos = line.find('#');
 			if (hashtag_pos != std::string::npos)
 				line.erase(hashtag_pos, std::string::npos);
 			if (!line.empty())
-				file_content.append(line + "\n");
+				_file_content.append(line + "\n");
 		}
 	}
-	
+
+	bool ConfigValidator::_validate_server_opening(std::string line)
+	{
+		std::string temp = line;
+		Utils::remove_white_space(temp);
+		if(temp.compare("server{") != 0)
+			throw ConfigException("Server opening line is invalid in config");
+		return true;
+	}
+
+	void ConfigValidator::_validate_server_blocks(void)
+	{
+		bool server_on;
+		std::string line;
+		std::istringstream stream(_file_content);
+
+		server_on = false;
+		while (std::getline(stream, line))
+		{
+			if (line.find("server") != std::string::npos && line.find("server_name") == std::string::npos)
+				server_on = _validate_server_opening(line);
+		}
+	}
 	//TODO it still parses without ; at the end of lines!
 	void ConfigValidator::validate(void)
 	{
-		open_and_read_file();
-		remove_comments();
-		// validate_config();
+		_open_and_read_file();
+		_remove_comments();
+		_validate_server_blocks();
 	}
 
 } // namespace Config
--- src/config/ConfigValidator.hpp
+++ src/config/ConfigValidator.hpp
@@ -17,16 +17,16 @@ namespace Config
 	{
 	private:
 		/* data */
-		std::string file_path;
-		std::string file_content;
+		std::string _file_path;
+		std::string _file_content;
 
 		/* methods */
-		void open_and_read_file(void);
-		void remove_comments(void);
-		// void validate_config(void);
+		void _open_and_read_file(void);
+		void _remove_comments(void);
+		void _validate_server_blocks(void);
+		bool _validate_server_opening(std::string line);
 		// bool validate_server_line(std::string line);
 		// void validate_location_block(std::string line, std::istringstream &stream);
-		// bool validate_bracket_opening(std::string line, const std::string& keyword);
 		// void check_outside_of_server_block(std::string line);
 		//TODO ConfigValidator(); do we need default constructor?
 
@@ -35,23 +35,5 @@ namespace Config
 		~ConfigValidator();
 
 		void validate(void);
-
-		// class FailedToOpenException : public std::exception
-		// {
-		// public:
-		// 	const char *what() const throw()
-		// 	{
-		// 		return "Configuration file failed to open";
-		// 	}
-		// };
-
-		// class InvalidConfigDirectiveException : public std::exception
-		// {
-		// public:
-		// 	const char *what() const throw()
-		// 	{
-		// 		return "Invalid directive in config";
-		// 	}
-		// };
 	};
 } // namespace Config
