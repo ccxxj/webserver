4cff0ea29614b6d980de5b1ea4f0b00452e2f638
ergunirem <iremthedeveloper@gmail.com>
2022-03-01 13:41:07 +0300

    empty file and no listen tests added

---
+++ tests/unit_tests/data_check_after_parse/data_check_after_parse.cpp
@@ -0,0 +1,36 @@
+#include "../catch_amalgamated.hpp"
+
+#include <string>
+#include <vector>
+
+#include "../../../src/config/ConfigParser.hpp"
+#include "../../../src/config/ConfigData.hpp"
+#include "../../../src/config/AConfigBlock.hpp"
+#include "../../../src/config/ServerBlock.hpp"
+#include "../../../src/config/LocationBlock.hpp"
+#include "../../../src/config/ConfigValidator.hpp"
+#include "../../../src/config/ConfigTokenizer.hpp"
+
+TEST_CASE("Empty conf")
+{
+	Config::ConfigValidator validator("data_check_after_parse/conf_files/empty_conf");
+	validator.validate();
+	Config::ConfigTokenizer tokenizer(validator.get_file_content());
+	tokenizer.tokenize_server_blocks();
+	Config::ConfigData config;
+	Config::ConfigParser parser(&config, tokenizer.get_server_tokens());
+	parser.parse();
+	CHECK_THROWS(config.check_parsed_data());
+}
+
+TEST_CASE("Missing listen line")
+{
+	Config::ConfigValidator validator("data_check_after_parse/conf_files/empty_conf");
+	validator.validate();
+	Config::ConfigTokenizer tokenizer(validator.get_file_content());
+	tokenizer.tokenize_server_blocks();
+	Config::ConfigData config;
+	Config::ConfigParser parser(&config, tokenizer.get_server_tokens());
+	parser.parse();
+	CHECK_THROWS(config.check_parsed_data());
+}
