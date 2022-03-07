933f75de8e393ae215aaa07390003741be0914c0
ergunirem <icikrikc@f1r2s15.codam.nl>
2022-03-07 11:49:50 +0100

    Utils functions changed to Utility

--- src/config/AConfigBlock.cpp
+++ src/config/AConfigBlock.cpp
@@ -26,7 +26,7 @@ namespace Config
 	{
 		std::vector<std::string> return_line;
 
-		return_line = Utils::split_string_white_space(str);
+		return_line = Utility::split_string_white_space(str);
 		if (return_line.size() < 2 || return_line.size() < 3)
 			throw std::runtime_error("invalid number of arguments in return directive");
 	}
@@ -34,18 +34,18 @@ namespace Config
     void AConfigBlock::set_return_value(std::string str)
     {
 		_check_return_syntax(str);
-        Utils::remove_first_keyword(str);
-        Utils::split_value(str, _return);
+        Utility::remove_first_keyword(str);
+        Utility::split_value(str, _return);
     }
 
     void AConfigBlock::set_root_value(std::string str)
     {
         if(!_root.empty())
             throw std::runtime_error("Invalid: root directive is duplicate");
-        Utils::remove_first_keyword(str);
+        Utility::remove_first_keyword(str);
         int first = str.find_first_not_of("     ;");
         int last = str.find_first_of("     ;", first + 1);
-        if(!Utils::check_after_keyword(last, str))
+        if(!Utility::check_after_keyword(last, str))
             throw std::runtime_error("Invalid: invalid number of arguments in root directive");
         _root = str.substr(first, last - first);
     }
