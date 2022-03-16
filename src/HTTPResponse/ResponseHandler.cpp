645f09de43a216e0b609f3e345d80421ca6a9b33
ergunirem <icikrikc@f1r5s5.codam.nl>
2022-03-16 15:17:33 +0100

    fixed abort error with location->get_route in process_http_request()

--- src/HTTPResponse/ResponseHandler.cpp
+++ src/HTTPResponse/ResponseHandler.cpp
@@ -7,31 +7,36 @@
 // #include "../config/ServerBlock.hpp"
 
 namespace HTTPResponse {
-	ResponseHandler::ResponseHandler(ResponseMessage* response_message)
-	// : _http_request_message()
-	: _http_response_message(response_message)
+	ResponseHandler::ResponseHandler(HTTPRequest::RequestMessage* request_message, ResponseMessage* response_message)
+	: _http_request_message(request_message)
+	, _http_response_message(response_message)
 	{
+		(void)_http_request_message; //TODO to silence flags. remove it.
+		(void)_http_response_message; //TODO to silence flags. remove it.
 	} //TODO do i need a copy const?
 
 	ResponseHandler::~ResponseHandler(){}
 
-	void ResponseHandler::create_http_response(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location) {
-		(void)virtual_server;
-		(void)location;
+	void ResponseHandler::create_http_response() {
         // if (!location)
         //     _verify_method(location->get_limit_except()); //HTTPResponse::MethodNotAllowed ? how to move from here?
         // checks & building the response
     }
 
-	void ResponseHandler::set_server(Config::ServerBlock* server) {
+
+	void ResponseHandler::set_server(const Config::ServerBlock* server) {
 		_server = server;
 	}
 
-	void ResponseHandler::set_location(Config::LocationBlock* location) {
+	void ResponseHandler::set_location(const Config::LocationBlock* location) {
 		_location = location;
 
 	}
 
+	void ResponseHandler::set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location) {
+		set_server(virtual_server);
+		set_location(location);
+	}
 	// bool ResponseHandler::_verify_method(const std::vector<std::string> methods) {
 	// 	const std::string request_method = _http_request_message.get_method();
 	// 	for (std::vector<std::string>::const_iterator it = methods.begin(); it != methods.end(); it++) {
--- src/HTTPResponse/ResponseHandler.hpp
+++ src/HTTPResponse/ResponseHandler.hpp
@@ -2,33 +2,30 @@
 
 #include <string>
 
-
 #include "StatusCodes.hpp"
+#include "../config/ServerBlock.hpp"
 #include "../HTTPRequest/RequestMessage.hpp"
-// #include "../config/ServerBlock.hpp"
-// #include "../config/LocationBlock.hpp"
-//
-#include "ResponseMessage.hpp" //webserv_response/src/HTTPResponse/
+#include "ResponseMessage.hpp"
 
 namespace HTTPResponse {
     class ResponseHandler {
     private:
-        HTTPRequest::RequestMessage* _http_request_message; //TODO have this and cgi as well.
+        HTTPRequest::RequestMessage* _http_request_message;
         ResponseMessage* _http_response_message; //TODO check if you change the real response msg cmoning from request handler!
-		Config::ServerBlock* _server;
-        Config::LocationBlock* _location;
+		const Config::ServerBlock* _server;
+        const Config::LocationBlock* _location;
 
         // void _handle_request_exception(StatusCode code);
         // const std::string _convert_status_code_to_string(const int code);
 
     public:
-        ResponseHandler(ResponseMessage* response_message);
+        ResponseHandler(HTTPRequest::RequestMessage* request_message, ResponseMessage* response_message);
         ~ResponseHandler();
 
-        void create_http_response(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location);
-
-		void set_server(Config::ServerBlock* server);
-		void set_location(Config::LocationBlock* location);
+        void create_http_response();
+		void set_server(const Config::ServerBlock* server);
+		void set_location(const Config::LocationBlock* location);
+        void set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location);
 		//TODO do i need getter if I think i wont need it  at all outside of the class?
     };
 }
