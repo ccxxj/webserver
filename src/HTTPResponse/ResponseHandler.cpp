406c701f451a07449033d078f9996d6e59643faf
ergunirem <iremthedeveloper@gmail.com>
2022-03-17 21:21:02 +0100

    set_config_rules & check_client_body_size funcs added

--- src/HTTPResponse/ResponseHandler.cpp
+++ src/HTTPResponse/ResponseHandler.cpp
@@ -17,25 +17,14 @@ namespace HTTPResponse {
 
 	ResponseHandler::~ResponseHandler(){}
 
-	const std::string& ResponseHandler::_create_allowed_methods_line(const std::vector<std::string> methods) { //FIXME can this be put somewhere else?
-		std::vector<std::string>::const_iterator it = methods.begin();
-		while (it != methods.end()) {
-			_http_response_message->append_methods(*it);
-			it++;
-			if (it != methods.end())
-				_http_response_message->append_methods(", ");
-		}
-		return _http_response_message->get_methods();
-	}
-
 	void ResponseHandler::create_http_response() {
 		std::cout << "inside create" << std::endl;
-		if (_location) {
+		if (_config.has_specific_location()) {
 			std::cout << "we have locccc matched" << std::endl;
-			if(!_verify_method(_location->get_limit_except())) { //TODO have this in one separate function?
+			if(!_verify_method(_config.get_limit_except())) { //TODO have this in one separate function?
 				handle_status_line(MethodNotAllowed);
 				std::cout << _http_response_message->get_status_line() << std::endl;
-				std::pair<std::string, std::string> header_field("Allow", _create_allowed_methods_line(_location->get_limit_except()));
+				std::pair<std::string, std::string> header_field("Allow", _config.get_methods_line());
 				_http_response_message->set_header_element(header_field);
 			}
 		}
@@ -46,16 +35,15 @@ namespace HTTPResponse {
 
 	bool ResponseHandler::_verify_method(const std::vector<std::string> methods) {
 		const std::string request_method = _http_request_message->get_method();
-		for (std::vector<std::string>::const_iterator it = methods.begin(); it != methods.end(); it++) {
+		for (std::vector<std::string>::const_iterator it = methods.begin(); it != methods.end(); it++)
 			if (request_method.compare(*it) == 0)
 				return true;
-		}
 		return false;
 	}
 
 	bool ResponseHandler::_check_client_body_size() {
-		size_t body_size = _http_request_message->get_message_body().size();
-		if (body_size > _server->get_client_max_body_size())
+		int body_size = _http_request_message->get_message_body().size(); //TODO what does size() return
+		if (body_size > _config.get_client_max_body_size())
 			return false;
 		return true;
 	}
@@ -77,16 +65,24 @@ namespace HTTPResponse {
 		return stringified_code;
 	}
 
-	void ResponseHandler::set_server(const Config::ServerBlock* server) {
-		_server = server;
-	}
+	void ResponseHandler::set_config_rules(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location) {
 
-	void ResponseHandler::set_location(const Config::LocationBlock* location) {
-		_location = location;
-	}
-
-	void ResponseHandler::set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location) {
-		set_server(virtual_server);
-		set_location(location);
+		_config.set_root_value(virtual_server->get_root());
+		_config.set_return_value(virtual_server->get_return());
+		_config.set_error_page_value(virtual_server->get_error_page());
+		_config.set_client_max_body_size(virtual_server->get_client_max_body_size());
+		if (location) //this replaces ServerBlock's config rules with LocationBlock's
+		{
+			_config.set_specific_location(true);
+			_config.set_limit_except(location->get_limit_except());
+			_config.set_methods_line(location->get_limit_except());
+			_config.set_route(location->get_route());
+			_config.set_autoindex(location->get_autoindex());
+			_config.set_root_value(location->get_root()); //does it replace it? if it's empty?
+			_config.set_return_value(location->get_return()); // does this add to values or replaces them
+			_config.set_error_page_value(location->get_error_page());
+			if (!location->get_is_size_default())
+				_config.set_client_max_body_size(location->get_client_max_body_size()); //TODO how do we know if loc has clientmax
+		}
 	}
 }
--- src/HTTPResponse/ResponseHandler.hpp
+++ src/HTTPResponse/ResponseHandler.hpp
@@ -6,6 +6,7 @@
 #include "../config/ServerBlock.hpp"
 #include "../HTTPRequest/RequestMessage.hpp"
 #include "ResponseMessage.hpp"
+#include "SpecifiedConfig.hpp"
 
 namespace HTTPResponse
 {
@@ -14,8 +15,7 @@ namespace HTTPResponse
 	private:
 		HTTPRequest::RequestMessage *_http_request_message;
 		ResponseMessage *_http_response_message; // TODO check if you change the real response msg cmoning from request handler!
-		const Config::ServerBlock *_server;
-		const Config::LocationBlock *_location;
+		SpecifiedConfig _config;
 
 		// void _handle_request_exception(StatusCode code);
 		// const std::string _convert_status_code_to_string(const int code);
@@ -30,9 +30,7 @@ namespace HTTPResponse
 		void create_http_response();
 		void handle_status_line(HTTPResponse::StatusCode code);
 		const std::string convert_status_code_to_string(const int code);
-		void set_server(const Config::ServerBlock *server);
-		void set_location(const Config::LocationBlock *location);
-		void set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location);
+		void set_config_rules(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location);
 		// TODO do i need getter if I think i wont need it  at all outside of the class?
 	};
 }
