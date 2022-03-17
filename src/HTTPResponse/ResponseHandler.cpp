fc1af150bbb6acaa1e424a4b9835dd0c3e8c61cf
ergunirem <iremthedeveloper@gmail.com>
2022-03-17 14:16:17 +0100

    verify_method and handle_status_line functions added

--- src/HTTPResponse/ResponseHandler.cpp
+++ src/HTTPResponse/ResponseHandler.cpp
@@ -18,11 +18,42 @@ namespace HTTPResponse {
 	ResponseHandler::~ResponseHandler(){}
 
 	void ResponseHandler::create_http_response() {
-        // if (!location)
-        //     _verify_method(location->get_limit_except()); //HTTPResponse::MethodNotAllowed ? how to move from here?
-        // checks & building the response
-    }
+		std::cout << "inside create" << std::endl;
+		if (_location) {
+			std::cout << "we have locccc matched" << std::endl;
+			if(!_verify_method(_location->get_limit_except())) {
+				handle_status_line(MethodNotAllowed);
+				std::cout << _http_response_message->get_status_line() << std::endl;
+			}
+		}
+		// checks & building the response
+	}
 
+	bool ResponseHandler::_verify_method(const std::vector<std::string> methods) {
+		const std::string request_method = _http_request_message->get_method();
+		for (std::vector<std::string>::const_iterator it = methods.begin(); it != methods.end(); it++) {
+			if (request_method.compare(*it) == 0)
+				return true;
+		}
+		return false;
+	}
+
+	void ResponseHandler::handle_status_line(HTTPResponse::StatusCode code) {
+		_http_response_message->set_status_code(convert_status_code_to_string(static_cast<int>(code)));
+		_http_response_message->set_reason_phrase(HTTPResponse::get_reason_phrase(code));
+		std::string status_line = _http_response_message->get_HTTP_version() + " ";
+		status_line += _http_response_message->get_status_code() + " ";
+		status_line += _http_response_message->get_reason_phrase(); + "\r\n\r\n";
+		_http_response_message->set_status_line(status_line);
+	}
+
+	const std::string ResponseHandler::convert_status_code_to_string(const int code) {
+		std::string stringified_code;
+		std::stringstream sstream;
+		sstream << code;
+		sstream >> stringified_code;
+		return stringified_code;
+	}
 
 	void ResponseHandler::set_server(const Config::ServerBlock* server) {
 		_server = server;
@@ -30,19 +61,10 @@ namespace HTTPResponse {
 
 	void ResponseHandler::set_location(const Config::LocationBlock* location) {
 		_location = location;
-
 	}
 
 	void ResponseHandler::set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location) {
 		set_server(virtual_server);
 		set_location(location);
 	}
-	// bool ResponseHandler::_verify_method(const std::vector<std::string> methods) {
-	// 	const std::string request_method = _http_request_message.get_method();
-	// 	for (std::vector<std::string>::const_iterator it = methods.begin(); it != methods.end(); it++) {
-	// 		if (request_method.compare(*it) == 0)
-	// 			return true;
-	// 	}
-	// 	return false;
-	// }
 }
--- src/HTTPResponse/ResponseHandler.hpp
+++ src/HTTPResponse/ResponseHandler.hpp
@@ -7,25 +7,30 @@
 #include "../HTTPRequest/RequestMessage.hpp"
 #include "ResponseMessage.hpp"
 
-namespace HTTPResponse {
-    class ResponseHandler {
-    private:
-        HTTPRequest::RequestMessage* _http_request_message;
-        ResponseMessage* _http_response_message; //TODO check if you change the real response msg cmoning from request handler!
-		const Config::ServerBlock* _server;
-        const Config::LocationBlock* _location;
+namespace HTTPResponse
+{
+	class ResponseHandler
+	{
+	private:
+		HTTPRequest::RequestMessage *_http_request_message;
+		ResponseMessage *_http_response_message; // TODO check if you change the real response msg cmoning from request handler!
+		const Config::ServerBlock *_server;
+		const Config::LocationBlock *_location;
 
-        // void _handle_request_exception(StatusCode code);
-        // const std::string _convert_status_code_to_string(const int code);
+		// void _handle_request_exception(StatusCode code);
+		// const std::string _convert_status_code_to_string(const int code);
+		bool _verify_method(const std::vector<std::string> methods);
 
-    public:
-        ResponseHandler(HTTPRequest::RequestMessage* request_message, ResponseMessage* response_message);
-        ~ResponseHandler();
+	public:
+		ResponseHandler(HTTPRequest::RequestMessage *request_message, ResponseMessage *response_message);
+		~ResponseHandler();
 
-        void create_http_response();
-		void set_server(const Config::ServerBlock* server);
-		void set_location(const Config::LocationBlock* location);
-        void set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location);
-		//TODO do i need getter if I think i wont need it  at all outside of the class?
-    };
+		void create_http_response();
+		void handle_status_line(HTTPResponse::StatusCode code);
+		const std::string convert_status_code_to_string(const int code);
+		void set_server(const Config::ServerBlock *server);
+		void set_location(const Config::LocationBlock *location);
+		void set_server_and_location(const Config::ServerBlock *virtual_server, const Config::LocationBlock *location);
+		// TODO do i need getter if I think i wont need it  at all outside of the class?
+	};
 }
