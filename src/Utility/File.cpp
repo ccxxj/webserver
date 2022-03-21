dc0dd4c05cbaed14aa7f25e301f86a2b9af92894
ergunirem <icikrikc@f1r5s5.codam.nl>
2022-03-21 19:01:57 +0100

    file.exists function added

--- src/Utility/File.cpp
+++ src/Utility/File.cpp
@@ -1,5 +1,7 @@
 #include "File.hpp"
-#include <sys/stat.h> //for stat that retrives information about a file
+#include <sys/stat.h> //for stat that retrives information about a file 
+
+//stat path check: relative to the current working directory of the calling process
 namespace Utility
 {
 
@@ -13,19 +15,25 @@ namespace Utility
 
 	void File::set_path(const std::string &root, const std::vector<std::string> &uri_paths) {
 		_path += root;
-		for (std::vector<std::string>::const_iterator it = uri_paths.begin(); it != uri_paths.end(); it++)
-			_path += (*it) + "/"; // FIXME what about the / in the end? check with directory and file
+		std::vector<std::string>::const_iterator it = uri_paths.begin(); 
+		while (it != uri_paths.end()) {
+			_path += *it;
+			it++;
+			if (it != uri_paths.end())
+				_path += "/";
+		}
+		// FIXME what about the / in the end? check with directory and file
 		//so the path is now "www/wordpress/index.html/"
 	}
 
-	bool File::is_directory(void) {
-		struct stat s;
-		if (stat(_path.c_str(), &s) == 0) // what happens if it doesn't exist? > should I check this first before everything else in serve_file
-		{
-			std::cout << s.st_mode << std::endl;
-			return S_ISDIR(s.st_mode);
-		}
-		return false; //FIXME
+	bool File::exists(void) {
+		struct stat buffer; 
+		return stat(_path.c_str(), &buffer) == 0;
 	}
 
+	bool File::is_directory(void) {
+		struct stat buffer; 
+		stat(_path.c_str(), &buffer);
+		return S_ISDIR(buffer.st_mode);
+	}
 }
--- src/Utility/File.hpp
+++ src/Utility/File.hpp
@@ -15,6 +15,7 @@ namespace Utility
 		~File();
 
 		void set_path(const std::string &root, const std::vector<std::string> &uri_paths);
+		bool exists(void);
 		bool is_directory(void);
 	};
 
