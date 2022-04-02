121e58b7260c873750d7f66b89a1498a403f7d3c
Mollie-S <olcha.olcha@gmail.com>
2022-04-02 20:25:26 +0200

    Handling multipart content_type

--- src/HTTPRequest/RequestParser.hpp
+++ src/HTTPRequest/RequestParser.hpp
@@ -2,6 +2,7 @@
 #define REQUESTPARSER_HPP
 
 #include <set>
+#include <vector>
 #include <iostream> // TODO: remove
 #include <sys/types.h>// for ssize_t
 
@@ -22,6 +23,7 @@ namespace HTTPRequest {
             HEADER,
             PAYLOAD,
             CHUNKED_PAYLOAD,
+            MULTIPART_PAYLOAD,
             TRAILER,
             FINISHED
         };
@@ -35,21 +37,28 @@ namespace HTTPRequest {
 
         RequestReader _request_reader;
         State _current_parsing_state;
-        MessageBodyLength _payload_length;
+        MessageBodyLength _payload_length_type;
+        size_t _payload_bytes_left_to_parse;
 
         ssize_t _chunk_size;
 		size_t _decoded_body_length;
 		std::string _decoded_body;
 
+		std::string _boundary;
+
         void _handle_request_message_part(std::string& line);
         void _parse_request_line(std::string& line);
         void _parse_header(std::string& line);
-        void _define_message_body_length();
+        void _validate_headers();
+        void _define_payload_length_type();
         bool _has_header_field(const std::string& header_name);
-        void _parse_transfer_encoding(std::string coding_names_list);
-        int _set_content_length();
+        void _check_multipart_content_type();
+        void _set_multipart_boundary(std::string& content_type_value);
+        void _parse_transfer_encoding(std::string &coding_names_list);
+        void _set_content_length();
         ssize_t _find_chunked_encoding_position(std::vector<std::string> &encodings, size_t encodings_num);
         void _parse_payload(std::string &line);
+        void _parse_multipart_payload(std::string &line);
         void _parse_trailer_header_fields(std::string &line);
         void _decode_chunked(std::string& line);
         void _set_chunk_size(std::string& line);
